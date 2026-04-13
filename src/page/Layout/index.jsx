da436e37716ac15c2d5d898297ac2c4c687b720c
“cwl” <1807682554@qq.com>
2026-04-13 17:08:15 +0800

    TabBar

--- src/page/Layout/index.jsx
+++ src/page/Layout/index.jsx
@@ -4,7 +4,32 @@ import { Link, Outlet } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux'
 import { getBillList } from '@/store/models/billstore'
 import { useEffect } from 'react'
+// 引入样式
+import { TabBar } from "antd-mobile"
+import './index.scss'
+import {
+    BillOutline,
+    CalculatorOutline,
+    AddCircleOutline
+} from 'antd-mobile-icons'
 
+const tabs = [
+    {
+        key: '/month',
+        title: '月度账单',
+        icon: <BillOutline />,
+    },
+    {
+        key: '/new',
+        title: '记账',
+        icon: <AddCircleOutline />,
+    },
+    {
+        key: '/year',
+        title: '年度账单',
+        icon: <CalculatorOutline />,
+    },
+]
 
 const Layout = () => {
     //获取账单列表
@@ -16,16 +41,17 @@ const Layout = () => {
     }, [dispatch])
 
     return (
-        <div>
-            这是一级路由
-            {/* 账单列表 */}
-            <ul>
-                {billList.map((item) => (
-                    <li key={item.id}>{item.money}</li>
-                ))}
-            </ul>
-            {/* 配置二级路由的出口 */}
-            <Outlet />
+        <div className="layout">
+            <div className="container">
+                <Outlet />
+            </div>
+            <div className="footer">
+                <TabBar>
+                    {tabs.map(item => (
+                        <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+                    ))}
+                </TabBar>
+            </div>
         </div>
     )
 }
